ccfe74872ae2f28f48ec0851b1fe9c1579678523
Robin <85845798+MeBeQuack@users.noreply.github.com>
2023-02-16 20:56:32 +0100

    Bugfix & general game-loop completion
    Changes:
    ⁕ Fixed input bug (where you could reverse your direction, i.e. turn left after facing right) ✓
    ⁕ Fixed fruit invisible bug (where the snake had erased the fruit after it had been drawn on the snake) ✓
    ⁕ Fixed that you gained way to many segments from eating a fruit (now you only gain one) ✓
    ⁕ Added customisable tab prefix ✓
    ⁕ Completed the fruit spawning loop (now the fruits keep spawning after you eat them) ✓
    ⁕ Changed score layout ✓
    ⁕ Correctly aligned the canvas with the top LEFT part of the screen (since it had previously been offset a bit to the left) ✓
    ⁕ NEED to fix tail collision death (so the snake dies on collision with it's tail) ✕

--- inject.js
+++ inject.js
@@ -1,51 +1,55 @@
 var t=0;
 var m=3;
-var l=5;
+var l=m;
 var n=60;
-var p=[n/2,n/2];
+var p=[30,30];
+var du=[t,-m];
+var dd=[t,m];
+var dl=[-m,t];
+var dr=[m,t];
 var d=[t,-m];
-var s=[[t,t]];
+var s=[p];
+var j = prompt("tab prefix")
 
-document.body.innerHTML+=`<canvas id="canvasIsMine" width="${n}" height="${n}" style="top:0;border:none;">c</canvas>`; 
+document.body.innerHTML+=`<canvas id="canvasIsMine" width="${n}" height="${n}" style="top:0;left:0;border:none;">c</canvas>`; 
 var c=document.getElementById("canvasIsMine"); 
 c.style.position="fixed"; 
 var x=c.getContext("2d");
 
 document.body.addEventListener("keydown",(e)=>{
-    if(e.key=="ArrowLeft"||e.key=="a"){d=[-m,0];}
-    if(e.key=="ArrowRight"||e.key=="d"){d=[m,0];}
-    if(e.key=="ArrowUp"||e.key=="w"){d=[0,-m];}
-    if(e.key=="ArrowDown"||e.key=="s"){d=[0,m];}
+    if(d!=dr&&(e.key=="ArrowLeft"||e.key=="a")){d=dl;}
+    if(d!=dl&&(e.key=="ArrowRight"||e.key=="d")){d=dr;}
+    if(d!=dd&&(e.key=="ArrowUp"||e.key=="w")){d=du;}
+    if(d!=du&&(e.key=="ArrowDown"||e.key=="s")){d=dd;}
 });
 var f=F();
 
 function D(){
-    document.title=`Total score: ${Math.floor(l*(t/80))}!`;
+    document.title= j +` ${l} : ${Math.floor(t*100/8)/100}`;
     c.remove();
 }
 function G(){
-    l+=m;
-    
+    l++;
+    return F();
 }
 function S(){
     p=[p[0]+d[0],p[1]+d[1]];
-    if (s.includes(p)||p[0]>n||p[1]>n||p[0]<0||p[1]<0)return D();
-    if(f[0]===p[0]&&f[1]===p[1])G();
+    if (p[0]>n||p[1]>n||p[0]<0||p[1]<0||s.some((e)=>e.includes(p)))
+    return D();
+    if(f[0]===p[0]&&f[1]===p[1])f=G();
     s.push([p]);
+    x.fillStyle="#F79F1F";
+    x.fillRect(f[0],f[1],m,m);
     x.fillStyle="#009432";
     x.fillRect(p[0],p[1],m,m);
     if(s.length>l){let g=s.shift()[0];x.clearRect(g[0],g[1],m,m);}
-    document.title=l*(t/80);
+    document.title= j+` ${l} : ${Math.floor(t/8)}`;
     t++;
-    var v=setTimeout((e)=>{
-        requestAnimationFrame(S);
-    },80)
+    var v=setTimeout((e)=>{requestAnimationFrame(S);},80)
     
 }
 function F(){
     var f=[Math.floor(Math.random()/m*n/2)*m,Math.floor(Math.random()/m*n/2)*m]
-    x.fillStyle="#F79F1F";
-    x.fillRect(f[0],f[1],m,m);
     return f;
 }
 
